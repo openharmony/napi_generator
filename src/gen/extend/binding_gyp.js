0f97647e45fbd955b4dad13651bcd7c4e51b7f6e
zhaoxudong <bayanxing@kaihongdigi.com>
2022-02-23 17:26:10 +0800

    modify binding_gyp.js
    Signed-off-by: zhaoxudong <bayanxing@kaihongdigi.com>

--- src/gen/extend/binding_gyp.js
+++ src/gen/extend/binding_gyp.js
@@ -34,6 +34,18 @@ let gypTemplete = `
 /**创建nodejs编译文件，用于在ubuntu测试 */
 function generateGYP(destDir, implName, license) {
     let ss = gypTemplete.replaceAll("[implName]", implName)
+    if (license) {
+        let s2 = license.substring(2, ss.length - 2).split("\n");
+        license = "";
+        for (let i = 1; i < s2.length; i++) {
+            if (s2[i].length > 0) {
+                while (s2[i][0] == " ") s2[i] = s2[i].substring(1);
+                if (s2[i].length > 3 && s2[i][0] == "*") {
+                    license += "#" + s2[i].substring(1) + "\n";
+                }
+            }
+        }
+    }
     writeFile(re.pathJoin(destDir, "binding.gyp"), null != license ? (license + "\n" + ss) : ss)
 
     writeFile(re.pathJoin(destDir, "test.sh"), "node-gyp configure build && sleep 0.5 && node --expose-gc test.js")
