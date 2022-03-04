57c190108cc1ce288e6f099c1e6df554b620b2fa
bayanxing <bayanxing@kaihongdigi.com>
2022-03-04 11:22:45 +0800

    modify IntelliJ0310
    Signed-off-by: bayanxing <bayanxing@kaihongdigi.com>

--- src/generator/src/com/sk/dialog/GenerateDialog.java
+++ src/generator/src/com/sk/dialog/GenerateDialog.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.sk.dialog;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -11,6 +25,11 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
 
+/**
+ * @author: xudong
+ * @see: 选择路径弹窗
+ * @version: 2022/02/21/v1.0.0
+ */
 public class GenerateDialog extends JDialog {
     private static final Logger LOG = Logger.getInstance(GenerateDialog.class);
     private static final String TITLE = "Generate Napi Frame";
@@ -81,8 +100,7 @@ public class GenerateDialog extends JDialog {
 
     private void onHelp() {
         try {
-            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler" +
-                    " https://gitee.com/openharmony-sig/napi_generator");
+            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler" + " https://gitee.com/openharmony-sig/napi_generator");
         } catch (IOException e) {
             LOG.error("exec command help error");
         }
@@ -188,10 +206,7 @@ public class GenerateDialog extends JDialog {
     }
 
     private void writeCommand() {
-        String command = "add_library(napitest SHARED x_napi_tool.cpp napitest.cpp napitest_middle.cpp)"
-                .replaceAll("napitest", fileName) + "\n" +
-                "target_link_libraries(napitest libace_napi.z.so)"
-                        .replaceAll("napitest", fileName);
+        String command = "add_library(napitest SHARED x_napi_tool.cpp napitest.cpp napitest_middle.cpp)".replaceAll("napitest", fileName) + "\n" + "target_link_libraries(napitest libace_napi.z.so)".replaceAll("napitest", fileName);
         FileUtil fileUtil = new FileUtil();
         String filePath = fileUtil.makeFile(directoryPath + "/makeFile.txt");
         if (TextUtils.isEmpty(filePath)) {
