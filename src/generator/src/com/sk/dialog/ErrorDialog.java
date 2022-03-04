29c6a3839aeb5984ac2ee2860f0be4ef6b9bb29a
zhaoxudong@kaihongdigi.com <zhaoxudong>
2022-03-04 11:22:45 +0800

    modify IntelliJ
    Signed-off-by: zhaoxudong@kaihongdigi.com <zhaoxudong>

--- src/generator/src/com/sk/dialog/ErrorDialog.java
+++ src/generator/src/com/sk/dialog/ErrorDialog.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.sk.dialog;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -6,6 +20,11 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.io.IOException;
 
+/**
+ * @author: xudong
+ * @see: 生成文件错误弹窗
+ * @version: 2022/02/21/v1.0.0
+ */
 public class ErrorDialog extends JDialog {
     private static final Logger LOG = Logger.getInstance(ErrorDialog.class);
     private JPanel contentPane;
