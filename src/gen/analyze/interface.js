2e9eb29c2db87d8977191193cce48405042f7e66
liuycag <liuycag@digitalchina.com>
2022-05-25 15:54:05 +0800

    feat: add map type
    fix: #I51BBW
    
    Signed-off-by: liuycag <liuycag@digitalchina.com>

--- src/gen/analyze/interface.js
+++ src/gen/analyze/interface.js
@@ -31,20 +31,22 @@ function analyzeInterface(data) {//same as class
         while (t.length > 0 && t[-1] == ' ')//去除后面的空格
             t = t.substring(0, t.length - 1)
         if (t == "") break//如果t为空直接返回
-        let tt = re.match(" *([a-zA-Z0-9_]+) *: *([a-zA-Z_0-9<>]+)", t)
+        let tt = re.match(" *([a-zA-Z0-9_]+) *: *([a-zA-Z_0-9<>,:{}[\\] ]+)", t)
         if (tt) {//变量
 
             let valueName = re.getReg(t, tt.regs[1])
             let valueType = re.getReg(t, tt.regs[2])
-            if (valueType.indexOf("number") >= 0) {
+            let index = valueType.indexOf("number")
+            while (index !== -1){
                 valueType = valueType.replace("number", "NUMBER_TYPE_" + NumberIncrease.getAndIncrease())
+                index = valueType.indexOf("number")
             }
             result.value.push({
                 name: valueName,
                 type: valueType
             })
         }
-        tt = re.match(" *([A-Za-z0-9_]+)\\(([\n a-zA-Z:;=,_0-9?<>{}|]*)\\) *: *([A-Za-z0-9_<>{}:, .]+)", t)
+        tt = re.match(" *([A-Za-z0-9_]+)\\(([\n a-zA-Z:;=,_0-9?<>{}|[\\]]*)\\) *: *([A-Za-z0-9_<>{}:, .[\\]]+)", t)
         if (tt) {//函数
             let funcDetail = analyzeFunction(re.getReg(t, tt.regs[1]),
                 re.getReg(t, tt.regs[2]), re.getReg(t, tt.regs[3]))
---
+++ test/storytest/test_map/@ohos.test.d.ts
@@ -0,0 +1,28 @@
+/*
+* Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd. 
+* Licensed under the Apache License, Version 2.0 (the "License"); 
+* you may not use this file except in compliance with the License. 
+* You may obtain a copy of the License at 
+*
+* http://www.apache.org/licenses/LICENSE-2.0 
+*
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License. 
+*/
+declare namespace napitest {
+    interface TestClass1 {
+        map1 : {[key: string]: number} ;
+        map2 : {[key: string]: string} ;
+        map3 : {[key: string]: boolean} ;
+    }
+
+    interface TestClass2 {
+        fun1(v: {[key: string]: string}): {[key: string]: string};
+        fun1(v: {[key: string]: number}): {[key: string]: number};
+        fun1(v: {[key: string]: boolean}): {[key: string]: boolean};
+    }
+}
+export default napitest;
