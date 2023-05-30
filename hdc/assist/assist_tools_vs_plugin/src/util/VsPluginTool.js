80080dc68f91e0f79c5a4849b4fc55cdeab00d71
zhaojunxia <zhaojunxia@kaihong.com>
2023-05-30 17:56:43 +0800

    style: modify service, assist vs plugin codecheck and add service selinux cfg doc
    Signed-off-by: zhaojunxia <zhaojunxia@kaihong.com>

--- hdc/assist/assist_tools_vs_plugin/src/util/VsPluginTool.js
+++ hdc/assist/assist_tools_vs_plugin/src/util/VsPluginTool.js
@@ -22,38 +22,52 @@ catch (err) {
 }
 
 function utf8ArrayToStr(array) {
-    var out, i, len, c;
-    var char2, char3;
+    var res, i, length, ch;
+    var ch2, ch3;
   
-    out = "";
-    len = array.length;
+    res = "";
+    length = array.length;
     i = 0;
-    while (i < len) {
-      c = array[i++];
-      switch (c >> 4) {
+    while (i < length) {
+      ch = array[i++];
+      let c = ch >> 4;
+      switch (c) {
+        // 0xxxxxxx
         case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-          // 0xxxxxxx
-          out += String.fromCharCode(c);
+          res += String.fromCharCode(ch);
           break;
+        // 110x xxxx   10xx xxxx
         case 12: case 13:
-          // 110x xxxx   10xx xxxx
-          char2 = array[i++];
-          out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+          ({ ch2, i, res } = funcCase13(ch2, array, i, ch, res));
           break;
+        // 1110 xxxx  10xx xxxx  10xx xxxx
         case 14:
-          // 1110 xxxx  10xx xxxx  10xx xxxx
-          char2 = array[i++];
-          char3 = array[i++];
-          out += String.fromCharCode(((c & 0x0F) << 12) |
-            ((char2 & 0x3F) << 6) |
-            ((char3 & 0x3F) << 0));
+          ({ ch2, i, ch3, res } = funcCase14(ch2, array, i, ch3, ch, res));
           break;
       }
     }
   
-    return out;
+    return res;
   }
   
+  function funcCase14(ch2, array, i, ch3, ch, res) {
+    ch2 = array[i++];
+    ch3 = array[i++];
+    let tmp3 = (ch & 0x0F) << 12;
+    let tmp4 = (ch2 & 0x3F) << 6;
+    let tmp5 = (ch3 & 0x3F) << 0;
+    res += String.fromCharCode(tmp3 | tmp4 | tmp5);
+    return { ch2, i, ch3, res };
+  }
+
+  function funcCase13(ch2, array, i, ch, res) {
+    ch2 = array[i++];
+    let tmp = (ch & 0x1F) << 6;
+    let tmp2 = ch2 & 0x3F;
+    res += String.fromCharCode(tmp | tmp2);
+    return { ch2, i, res };
+  }
+
   function readFile(fn) {
     if (!fs.existsSync(fn)) {
       return "";
