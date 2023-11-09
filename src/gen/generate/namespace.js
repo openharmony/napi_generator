def604dfa1a85c24f0f509a584ab6edd6dc576bf
gou-jingjing <goujingjing@kaihong.com>
2023-11-09 10:09:16 +0800

    add promise cb interface
    Signed-off-by: gou-jingjing <goujingjing@kaihong.com>

--- src/gen/generate/namespace.js
+++ src/gen/generate/namespace.js
@@ -148,7 +148,7 @@ function generateNamespace(name, data, inNamespace = "") {
 
 function genNamespaceFunc(data, i, namespaceResult, inNamespace, name) {
     let func = data.function[i];
-    let tmp = generateFunction(func, data);
+    let tmp = generateFunction(func, data, namespaceResult.implH);
     namespaceResult.middleFunc += tmp[0];
     namespaceResult.implH += tmp[1];
     namespaceResult.implCpp += tmp[2];
@@ -235,8 +235,9 @@ function generateResult(name, implH, implCpp, middleFunc, middleInit, middleH, f
   return result;
 }
 
-function generateFunction(func, data) {
+function generateFunction(func, data, implH = null) {
     let tmp;
+    let className;
     if (func.name == 'on' || func.name == 'off' || isRegisterFunc(func.name) || isUnRegisterFunc(func.name)) {
         return generateFunctionOnOff(func, data)
     }
@@ -249,7 +250,7 @@ function generateFunction(func, data) {
             break
         case FuncType.ASYNC:
         case FuncType.PROMISE:
-            tmp = generateFunctionAsync(func, data)
+            tmp = generateFunctionAsync(func, data, className, implH)
             break
         default:
             return
