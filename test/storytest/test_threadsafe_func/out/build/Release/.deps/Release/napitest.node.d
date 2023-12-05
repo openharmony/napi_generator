246836644a28308be6755e5a7ef5fb4bbd39cc92
zhaojunxia <zhaojunxia@kaihong.com>
2023-12-04 23:03:48 -0800

    modify tdsafeFunc
    Signed-off-by: zhaojunxia <zhaojunxia@kaihong.com>

--- test/storytest/test_threadsafe_func/out/build/Release/.deps/Release/napitest.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/napitest.node := ln -f "Release/obj.target/napitest.node" "Release/napitest.node" 2>/dev/null || (rm -rf "Release/napitest.node" && cp -af "Release/obj.target/napitest.node" "Release/napitest.node")
