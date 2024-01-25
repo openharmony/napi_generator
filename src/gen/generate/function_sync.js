740419ca06b5fcc44c01d72670077ed1ddf6e020
zhaojunxia <zhaojunxia@kaihong.com>
2024-01-24 22:01:11 -0800

    modify ==
    Signed-off-by: zhaojunxia <zhaojunxia@kaihong.com>

--- src/gen/generate/function_sync.js
+++ src/gen/generate/function_sync.js
@@ -191,7 +191,7 @@ function fillValueStruct(type, param, outName) {
         if (param.callback.returnType === 'void') {
             param.valueDefine += "%sbool& %s".format(param.valueDefine.length > 0 ? ", " : "", outName)
         }  
-    } else if (type.substring(0, 12) == "NUMBER_TYPE_") {
+    } else if (type.substring(0, 12) === "NUMBER_TYPE_") {
         param.valueOut += '%s %s;\n'.format(type, outName)
         if (param.callback.returnType === 'void') {
             param.valueDefine += "%s%s& %s".format(param.valueDefine.length > 0 ? ", " : "", type, outName)
