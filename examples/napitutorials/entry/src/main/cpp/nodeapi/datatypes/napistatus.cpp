5ca7b71509ee57c0d4c01d0d28af9444854c8b90
wangshi <wangshi@kaihong.com>
2024-03-25 14:17:21 +0800

    fix code check
    Signed-off-by: wangshi <wangshi@kaihong.com>

--- examples/napitutorials/entry/src/main/cpp/nodeapi/datatypes/napistatus.cpp
+++ examples/napitutorials/entry/src/main/cpp/nodeapi/datatypes/napistatus.cpp
@@ -17,7 +17,8 @@
 
 static const char *TAG = "[nodeapi_status]";
 
-napi_value testNapiStatus(napi_env env, napi_callback_info info) {
+napi_value testNapiStatus(napi_env env, napi_callback_info info)
+{
     // pages/nodeapi/datatypes/napistatus
     size_t argc = 2;
     napi_value argv[2];
