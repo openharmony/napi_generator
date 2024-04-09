711594bf0bd283398d8b2873a1cda96a68580c73
gou-jingjing <goujingjing@kaihong.com>
2024-04-09 10:08:05 +0800

    fix: resolve conflict
    Signed-off-by: gou-jingjing <goujingjing@kaihong.com>

--- examples/napitutorials/entry/src/main/cpp/types/libentry/index.d.ts
+++ examples/napitutorials/entry/src/main/cpp/types/libentry/index.d.ts
@@ -33,3 +33,7 @@ export const instance;
 
 export const cjson_version: () => string;
 export const testNapiGetPropertyNames: (a: {[key: string]: number}) => string;
+export const testNapiSetProperty: (a: object) => string;
+export const testNapiGetProperty: (a: object) => string;
+export const testNapiHasProperty: (a: object) => boolean;
+export const testNapiDeleteProperty: (a: object) => boolean;
