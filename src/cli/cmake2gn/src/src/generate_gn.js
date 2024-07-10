a43cda919bbf9d8c85790a0089dde4f66501b9fc
huruitao <huruitao@kaihong.com>
2024-07-10 10:02:39 +0800

    "modifile src/cli/cmake2gn/src/src/generate_gn.js 修改js格式问题"
    Signed-off-by: huruitao <huruitao@kaihong.com>

--- src/cli/cmake2gn/src/src/generate_gn.js
+++ src/cli/cmake2gn/src/src/generate_gn.js
@@ -12,24 +12,24 @@
 * See the License for the specific language governing permissions and 
 * limitations under the License. 
 */
-const path = require("path");
-const fs = require("fs");
-const { Logger } = require("./logger");
-const { Tool } = require("./tool");
-const { exit } = require("process");
+const path = require('path');
+const fs = require('fs');
+const { Logger } = require('./logger');
+const { Tool } = require('./tool');
+const { exit } = require('process');
 
 function saveMockData(projectPath, analyzeResult) {
     let ss = JSON.stringify({
         projectPath: projectPath,
         analyzeResult: analyzeResult
     }, null, 4);
-    fs.writeFileSync(path.join(Tool.CURRENT_TOOL_PATH, "mock.json"), ss);
-    Logger.err("save mock exit");
+    fs.writeFileSync(path.join(Tool.CURRENT_TOOL_PATH, 'mock.json'), ss);
+    Logger.err('save mock exit');
 }
 
 function preProcessResult(analyzeResult) {//把所有路径搞成绝对路径
     for (let r of analyzeResult) {
-        if (!r.target.startsWith("/")) {
+        if (!r.target.startsWith('/')) {
             if (!path.isAbsolute(r.target)) {
                 r.target = path.join(r.workDir, r.target);
             }
@@ -38,14 +38,14 @@ function preProcessResult(analyzeResult) {//把所有路径搞成绝对路径
             if (r.inputs[i].startsWith('"') && r.inputs[i].endsWith('"')) {
                 r.inputs[i] = r.inputs[i].substring(1, r.inputs[i].length - 1);
             }
-            if (!r.inputs[i].startsWith("/")) {
+            if (!r.inputs[i].startsWith('/')) {
                 if (!path.isAbsolute(r.inputs[i])) {
                     r.inputs[i] = path.join(r.workDir, r.inputs[i]);
                 }
             }
         }
         for (let i = 0; i < r.includes.length; i++) {
-            if (!r.includes[i].startsWith("/")) {
+            if (!r.includes[i].startsWith('/')) {
                 if (!path.isAbsolute(r.includes[i])) {
                     r.includes[i] = path.join(r.workDir, r.includes[i]);
                 }
@@ -57,22 +57,22 @@ function preProcessResult(analyzeResult) {//把所有路径搞成绝对路径
 function checkoutLibName(name) {//比如/home/libabc.so，返回["dynamic",abc]
     let pn = path.parse(name);
     let tname = pn.base;
-    if (tname.endsWith(".a")) {
+    if (tname.endsWith('.a')) {
         tname = tname.substring(0, tname.length - 2);
-        if (tname.startsWith("lib")) {
+        if (tname.startsWith('lib')) {
             tname = tname.substring(3);
         }
-        return ["static", tname];
+        return ['static', tname];
     }
-    else if (tname.endsWith(".so") || tname.indexOf(".so.") > 0) {
-        tname = tname.substring(0, tname.indexOf(".so"));
-        if (tname.startsWith("lib")) {
+    else if (tname.endsWith('.so') || tname.indexOf('.so.') > 0) {
+        tname = tname.substring(0, tname.indexOf('.so'));
+        if (tname.startsWith('lib')) {
             tname = tname.substring(3);
         }
-        return ["dynamic", tname];
+        return ['dynamic', tname];
     }
     else {
-        return ["executable", tname];
+        return ['executable', tname];
     }
 }
 
@@ -81,10 +81,10 @@ class GenerateGn {
     }
 
     static mockGenerate() {
-        let ss = fs.readFileSync(path.join(Tool.CURRENT_TOOL_PATH, "mock.json"), { encoding: "utf8" });
+        let ss = fs.readFileSync(path.join(Tool.CURRENT_TOOL_PATH, 'mock.json'), { encoding: 'utf8' });
         let ret = JSON.parse(ss);
         GenerateGn.generate(ret.projectPath, ret.analyzeResult);
-        Logger.err("generate mock exit");
+        Logger.err('generate mock exit');
     }
 
     static generate(projectPath, analyzeResult) {
@@ -107,12 +107,12 @@ class GenerateGn {
         }
         let num = 0;
         for (let gnPath in genList) {//genList的key即为需要生成gn的目录
-            Logger.info("%d-------------------generate dir %s".format(num, gnPath));
+            Logger.info('%d-------------------generate dir %s'.format(num, gnPath));
             GenerateGn.generateGn(gnPath, genList, projectPath);
             num++;
         }
         GenerateGn.generateTargetGroup(projectPath);
-        Logger.info("-------------------generate gn ok");
+        Logger.info('-------------------generate gn ok');
     }
     static COLLECT_TARGET = {
         static: [],
@@ -120,23 +120,23 @@ class GenerateGn {
         executable: []
     };
     static generateTargetGroup(projectPath) {
-        let gnName = path.join(projectPath, "BUILD.gn");
+        let gnName = path.join(projectPath, 'BUILD.gn');
         let gnStr = 'import("//build/ohos.gni")\n\n';
         if (fs.existsSync(gnName)) {
-            gnStr = fs.readFileSync(gnName, { encoding: "utf8" });
+            gnStr = fs.readFileSync(gnName, { encoding: 'utf8' });
         }
 
         let staticTargets = [];
         for (let t of GenerateGn.COLLECT_TARGET.static) {
-            staticTargets.push(Tool.swapPath(t.path) + ":" + t.name);
+            staticTargets.push(Tool.swapPath(t.path) + ':' + t.name);
         }
         let dynamicTargets = [];
         for (let t of GenerateGn.COLLECT_TARGET.dynamic) {
-            dynamicTargets.push(Tool.swapPath(t.path) + ":" + t.name);
+            dynamicTargets.push(Tool.swapPath(t.path) + ':' + t.name);
         }
         let executableTargets = [];
         for (let t of GenerateGn.COLLECT_TARGET.executable) {
-            executableTargets.push(Tool.swapPath(t.path) + ":" + t.name);
+            executableTargets.push(Tool.swapPath(t.path) + ':' + t.name);
         }
 
         gnStr += `
@@ -165,17 +165,17 @@ group("all_targets") {
 }
 `;
         while (gnStr.indexOf(Tool.OHOS_PROJECT_PATH) >= 0) {
-            gnStr = gnStr.replace(Tool.OHOS_PROJECT_PATH, "/");
+            gnStr = gnStr.replace(Tool.OHOS_PROJECT_PATH, '/');
         }
-        fs.writeFileSync(gnName, gnStr, { encoding: "utf8" });
+        fs.writeFileSync(gnName, gnStr, { encoding: 'utf8' });
     }
     static genTargetStr(targetName) {
         switch (targetName[0]) {
-            case "static":
+            case 'static':
                 return 'ohos_static_library("' + targetName[1] + '")';
-            case "dynamic":
+            case 'dynamic':
                 return 'ohos_shared_library("' + targetName[1] + '")';
-            case "executable":
+            case 'executable':
                 return 'ohos_executable("' + targetName[1] + '")';
         }
     }
@@ -195,18 +195,18 @@ group("all_targets") {
     }
     static genTargetStr2(collectDetails, targetStr, targetName) {
         if (collectDetails.cflags.size > 0 || collectDetails.cflagsCc.size > 0) {//放到config里面才生效
-            let configDetail = "";
-            let removeConfigs = "";
+            let configDetail = '';
+            let removeConfigs = '';
             if (collectDetails.cflags.size > 0) {
-                configDetail += GenerateGn.genDetail("cflags", collectDetails.cflags);
+                configDetail += GenerateGn.genDetail('cflags', collectDetails.cflags);
             }
             if (collectDetails.cflagsCc.size > 0) {
-                configDetail += GenerateGn.genDetail("cflags_cc", collectDetails.cflagsCc);
+                configDetail += GenerateGn.genDetail('cflags_cc', collectDetails.cflagsCc);
             }
-            if (collectDetails.cflags.has("-frtti")) {
+            if (collectDetails.cflags.has('-frtti')) {
                 removeConfigs += `        "//build/config/compiler:no_rtti",\n`;
             }
-            if (collectDetails.cflags.has("-fexceptions")) {
+            if (collectDetails.cflags.has('-fexceptions')) {
                 removeConfigs += `        "//build/config/compiler:no_exceptions",\n`;
             }
             targetStr = `config("%s_config") {%s}
@@ -218,16 +218,16 @@ configs = [ ":%s_config" ]
 `.format(targetName[1], configDetail, targetStr, removeConfigs, targetName[1]);
         }
         if (collectDetails.sources.size > 0) {
-            targetStr += GenerateGn.genDetail("sources", collectDetails.sources);
+            targetStr += GenerateGn.genDetail('sources', collectDetails.sources);
         }
         if (collectDetails.includeDirs.size > 0) {
-            targetStr += GenerateGn.genDetail("include_dirs", collectDetails.includeDirs);
+            targetStr += GenerateGn.genDetail('include_dirs', collectDetails.includeDirs);
         }
         if (collectDetails.defines.size > 0) {
-            targetStr += GenerateGn.genDetail("defines", collectDetails.defines);
+            targetStr += GenerateGn.genDetail('defines', collectDetails.defines);
         }
         if (collectDetails.deps.size > 0) {
-            targetStr += GenerateGn.genDetail("deps", collectDetails.deps);
+            targetStr += GenerateGn.genDetail('deps', collectDetails.deps);
         }
 
         targetStr += `
@@ -240,7 +240,7 @@ subsystem_name = "%s"
     }
     static generateGn(gnPath, genList, projectPath) {
         if (!gnPath.startsWith(projectPath)) {
-            Logger.err("target path not in project path\ntarget:%s\nproject:%s".format(gnPath, projectPath));
+            Logger.err('target path not in project path\ntarget:%s\nproject:%s'.format(gnPath, projectPath));
         }
         let gnStr = 'import("//build/ohos.gni")\n\n';
         let targetCount = 0;
@@ -263,17 +263,17 @@ subsystem_name = "%s"
         }
 
         if (targetCount > 0) {
-            let gnName = path.join(gnPath, "BUILD.gn");
-            Logger.info("输出：" + gnName + "\n" + gnStr);
-            fs.writeFileSync(gnName, gnStr, { encoding: "utf8" });
+            let gnName = path.join(gnPath, 'BUILD.gn');
+            Logger.info('输出：' + gnName + '\n' + gnStr);
+            fs.writeFileSync(gnName, gnStr, { encoding: 'utf8' });
         }
         else {
-            Logger.info("          no target");
+            Logger.info('          no target');
         }
     }
 
     static genDetail(name, detail) {
-        let ss = "";
+        let ss = '';
         for (let s of detail) {
             s=Tool.swapPath(s);
             if (ss.length > 0) ss += '",\n        "';
@@ -286,7 +286,7 @@ subsystem_name = "%s"
 `.format(name, ss);
 
         while (ret.indexOf(Tool.OHOS_PROJECT_PATH) >= 0) {
-            ret = ret.replace(Tool.OHOS_PROJECT_PATH, "/");
+            ret = ret.replace(Tool.OHOS_PROJECT_PATH, '/');
         }
         return ret;
     }
@@ -297,7 +297,7 @@ subsystem_name = "%s"
             for (let gen of gens) {
                 if (gen.target == name) {
                     let tt = checkoutLibName(gen.target);
-                    return gen.workDir + ":" + tt[1];
+                    return gen.workDir + ':' + tt[1];
                 }
             }
         }
@@ -310,23 +310,23 @@ subsystem_name = "%s"
             let badd = true;
             switch (collectFileStat) {
                 case 0:
-                    if (a == "-Xclang") {
+                    if (a == '-Xclang') {
                         collectFileStat = 1;
                         badd = false;
                     }
                     break;
                 case 1:
-                    if (a == "-include" || a == "-include-pch") {
+                    if (a == '-include' || a == '-include-pch') {
                         collectFileStat = 2;
                         badd = false;
                     }
                     else {
-                        collectDetails.cflags.add("-Xclang");
+                        collectDetails.cflags.add('-Xclang');
                         collectFileStat = 0;
                     }
                     break;
                 case 2:
-                    if (a == "-Xclang") {
+                    if (a == '-Xclang') {
                         collectFileStat = 3;
                         badd = false;
                     }
