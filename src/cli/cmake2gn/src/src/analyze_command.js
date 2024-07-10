98f2bd8aea30597dd5ee2fd82cd76c6c53f54711
gou-jingjing <goujingjing@kaihong.com>
2024-07-10 14:29:32 +0800

    fix codecheck 3
    Signed-off-by: gou-jingjing <goujingjing@kaihong.com>

--- src/cli/cmake2gn/src/src/analyze_command.js
+++ src/cli/cmake2gn/src/src/analyze_command.js
@@ -82,7 +82,7 @@ class AnalyzeCommand {
             let cmakeLinkScriptData = fs.readFileSync(path.join(process.cwd(), cmakeLinkScriptFile),
                 { encoding: 'utf8' });
             let cmds = cmakeLinkScriptData.split('\n');//link.txt中可能有多条命令链接
-            let rets = []
+            let rets = [];
             for (let c of cmds) {
                 let r = AnalyzeCommand.analyzeOneCmd(c);
                 if (r) {
@@ -102,7 +102,7 @@ class AnalyzeCommand {
         let cmdName = cmd.split(' ')[0];
         let pos = cmdName.lastIndexOf('/');
         let scrType = cmdName.substring(pos + 1, cmdName.length);
-        return scrType.startsWith(cmdType)
+        return scrType.startsWith(cmdType);
     }
 
     static analyzeOneCmd(cmd) {
@@ -176,7 +176,7 @@ class AnalyzeCommand {
             ],//c和c++选项
             cflagsCc: [],//c++选项
             cflagsC: [],//c选项
-        }
+        };
     }
 
     static splitString(s) {//按空格分割字符串
@@ -359,7 +359,7 @@ class AnalyzeCommand {
                 process.exit();
             }
         }
-        Logger.info('----clang-----' + local.ret.workDir + '\n\t' + local.ret.isLink + ',' + local.ret.target)
+        Logger.info('----clang-----' + local.ret.workDir + '\n\t' + local.ret.isLink + ',' + local.ret.target);
         return local.ret;
     }
     static analyzeCcAr(cmd) {
@@ -587,7 +587,7 @@ class AnalyzeCommand {
                 process.exit();
             }
         }
-        Logger.info('---clang++----' + local.ret.workDir + '\n\t' + local.ret.isLink + ',' + local.ret.target)
+        Logger.info('---clang++----' + local.ret.workDir + '\n\t' + local.ret.isLink + ',' + local.ret.target);
         return local.ret;
     }
 
@@ -642,4 +642,4 @@ function clangCheck7RspEnds(e, local) {
 
 module.exports = {
     AnalyzeCommand
-}
+};
