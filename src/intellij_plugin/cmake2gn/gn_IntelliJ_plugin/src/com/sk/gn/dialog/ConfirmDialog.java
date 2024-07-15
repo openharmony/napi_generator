a12d521782b7157c111907a23bebb3b44326017a
zhangmenghan <zhangmenghan@kaihong.com>
2024-07-15 17:47:12 +0800

    src/intellij_plugin/cmake2gn/gn_IntelliJ_plugin/src/com/sk/gn java
    Signed-off-by: zhangmenghan <zhangmenghan@kaihong.com>

--- src/intellij_plugin/cmake2gn/gn_IntelliJ_plugin/src/com/sk/gn/dialog/ConfirmDialog.java
+++ src/intellij_plugin/cmake2gn/gn_IntelliJ_plugin/src/com/sk/gn/dialog/ConfirmDialog.java
@@ -1,11 +1,10 @@
 /*
- * Copyright (c) 2022 Guangzhou Digitalchina Information Technology Co., Ltd.
- * All rights reserved.
+ * Copyright (c) 2024 Shenzhen Kaihong Digital Industry Development Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.sk.gn.dialog;
 
 import com.intellij.openapi.ui.DialogWrapper;
@@ -32,6 +32,7 @@ public class ConfirmDialog extends DialogWrapper {
 
     /**
      * 构造函数
+     * 
      * @param message 弹出框信息内容
      */
     public ConfirmDialog(String message) {
