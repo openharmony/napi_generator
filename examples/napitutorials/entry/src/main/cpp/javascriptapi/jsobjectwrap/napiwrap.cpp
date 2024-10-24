766b2a05b9f3a4b37ff1b16a284e0d31f2a340ba
zhangmenghan <zhangmenghan@kaihong.com>
2024-10-24 17:41:29 +0800

    codecheck
    Signed-off-by: zhangmenghan <zhangmenghan@kaihong.com>

--- examples/napitutorials/entry/src/main/cpp/javascriptapi/jsobjectwrap/napiwrap.cpp
+++ examples/napitutorials/entry/src/main/cpp/javascriptapi/jsobjectwrap/napiwrap.cpp
@@ -18,9 +18,11 @@
 
 static const char *TAG = "[javascriptapi_object_wrap]";
 static const int MAX_BUFFER_SIZE = 128;
+
 class Node {
 public:
-    Node(napi_env env, napi_value id) {
+    Node(napi_env env, napi_value id)
+    {
         // 将 JavaScript 字符串转换为 C++ 字符串
         size_t idLength = MAX_BUFFER_SIZE;
         char buf[MAX_BUFFER_SIZE];
@@ -30,19 +32,18 @@ public:
         _id = std::string(buffer);
     }
     std::string GetId() { return _id; }
-
 private:
     std::string _id; // 成员变量，存储 id
 };
 
-napi_value testNapiWrap(napi_env env, napi_callback_info info) {
+napi_value testNapiWrap(napi_env env, napi_callback_info info)
+{
     size_t argc = PARAM1;
     napi_value argv[PARAM1] = {0};
     napi_value thisObj = nullptr;
     void *data = nullptr;
     napi_status status;
     napi_value cons;
-
     const napi_extended_error_info *extended_error_info;
     // 获取回调函数的参数信息
     status = napi_get_cb_info(env, info, &argc, argv, &thisObj, &data);
@@ -58,15 +59,13 @@ napi_value testNapiWrap(napi_env env, napi_callback_info info) {
         return NULL;
     }
     auto instance = new Node(env, argv[PARAM0]);
-    status = napi_wrap(
-        env, thisObj, instance,
+    status = napi_wrap(env, thisObj, instance,
         [](napi_env environment, void *data, void *hint) {
             auto objInfo = reinterpret_cast<Node *>(data);
             if (objInfo != nullptr) {
                 delete objInfo;
             }
-        },
-        NULL, NULL);
+        }, NULL, NULL);
     if (status != napi_ok) {
         getErrMsg(status, env, extended_error_info, "wrap", TAG);
         return NULL;
