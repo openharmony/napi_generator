cd4828bfd96e3cdcb694be8f301bc6016546b0ee
wangshi <wangshi@kaihong.com>
2025-03-05 16:49:39 +0800

    add grammar test
    Signed-off-by: wangshi <wangshi@kaihong.com>

---
+++ src/intellij_plugin/ohosgen/src/test/java/grammar/ParamObjTest.java
@@ -0,0 +1,59 @@
+package grammar;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <h3>类名：该类用于xxx</h3>
+ * description
+ *
+ * @author Administrator
+ *         date 2025-02-28
+ * @version 1.0
+ * @since 2025-02-28
+ */
+class ParamObjTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void setType() {
+    }
+
+    @Test
+    void getArraySize() {
+    }
+
+    @Test
+    void getAsList() {
+    }
+
+    @Test
+    void setArraySize() {
+    }
+
+    @Test
+    void setAsList() {
+    }
+}
