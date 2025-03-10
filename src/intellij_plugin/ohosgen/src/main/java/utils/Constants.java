34266d644395cbdc6eb2ec72fe6fcad7d891faa9
wangshi <wangshi@kaihong.com>
2025-03-10 17:13:47 +0800

    add constants
    Signed-off-by: wangshi <wangshi@kaihong.com>

--- src/intellij_plugin/ohosgen/src/main/java/utils/Constants.java
+++ src/intellij_plugin/ohosgen/src/main/java/utils/Constants.java
@@ -25,4 +25,64 @@ package utils;
  * @version 1.0
  */
 public class Constants {
+    public static final int PARSE_C_CPP_LANGUAGE = 1;
+    public static final int PARSE_TS_LANGUAGE = 2;
+
+    public static final int PARSE_TS_ABSTRACT = 1;
+    public static final int PARSE_TS_CLASS = 2;
+    public static final int PARSE_TS_ENUM = 3;
+    public static final int PARSE_TS_EXPORT = 4;
+    public static final int PARSE_TS_FUNCTION = 5;
+    public static final int PARSE_TS_GENERIC = 6;
+    public static final int PARSE_TS_GENERIC_CLASS = 7;
+    public static final int PARSE_TS_GENERIC_INTERFACE = 8;
+    public static final int PARSE_TS_IMPORT = 9;
+    public static final int PARSE_TS_INTERFACE = 10;
+    public static final int PARSE_TS_JS_CLASS = 11;
+    public static final int PARSE_TS_LOOP = 12;
+    public static final int PARSE_TS_MODULE = 13;
+    public static final int PARSE_TS_NON_NULL = 14;
+    public static final int PARSE_TS_OBJECT_INITIALIZER = 15;
+    public static final int PARSE_TS_STATEMENT = 16;
+    public static final int PARSE_TS_TEMPLATE_STRING = 17;
+    public static final int PARSE_TS_TYPE = 18;
+    public static final int PARSE_TS_VARIABLE = 19;
+    public static final int PARSE_TS_EXIT_TRANSLATION = 20;
+
+    public static final int PARSE_C_CPP_CLASS = 51;
+    public static final int PARSE_C_CPP_ATTRIBUTE = 52;
+    public static final int PARSE_C_CPP_MEMBER = 53;
+    public static final int PARSE_C_CPP_FUNCTION = 54;
+    public static final int PARSE_C_CPP_ENUM = 55;
+    public static final int PARSE_C_CPP_TYPE = 56;
+    public static final int PARSE_C_CPP_TEMPLATE = 57;
+    public static final int PARSE_C_CPP_UNION = 58;
+    public static final int PARSE_C_CPP_STRUCT = 59;
+    public static final int PARSE_C_CPP_MACRO = 60;
+    public static final int PARSE_C_CPP_POINT = 61;
+    public static final int PARSE_C_CPP_PURE = 62;
+    public static final int PARSE_C_CPP_EXIT_TRANSLATION = 63;
+
+    public static final int TEN_PERCENT = 10;
+    public static final int HUNDRED_PERCENT = 100;
+    public static final int THOUSAND_PERCENT = 1000;
+
+    public static final String START_STATUS = "start";
+    public static final String PAUSE_STATUS = "pause";
+    public static final String RESUME_STATUS = "resume";
+    public static final String STOP_STATUS = "stop";
+    public static final String COMPLETE_STATUS = "complete";
+    public static final String FINISH_STATUS = "finish";
+
+    public static final String C_CPP_START_MSG = "c/cpp parse char stream start";
+    public static final String TS_START_MSG = "ts parse char stream start";
+    public static final String C_CPP_PAUSE_MSG = "c/cpp parse char stream pause";
+    public static final String TS_PAUSE_MSG = "ts parse char stream pause";
+    public static final String C_CPP_RESUME_MSG = "c/cpp parse char stream resume";
+    public static final String TS_RESUME_MSG = "ts parse char stream resume";
+    public static final String C_CPP_FINISH_MSG = "c/cpp parse char stream finish";
+    public static final String TS_FINISH_MSG = "ts parse char stream finish";
+    public static final String C_CPP_COMPLETE_MSG = "c/cpp parse char stream complete";
+    public static final String TS_COMPLETE_MSG = "ts parse char stream complete";
+
 }
