dc84c4a5b449b7d55478142e28a2814335268ca7
wangshi <wangshi@kaihong.com>
2025-03-12 17:37:28 +0800

    add token
    Signed-off-by: wangshi <wangshi@kaihong.com>

---
+++ src/intellij_plugin/ohosgen/src/main/java/utils/TsToken.java
@@ -0,0 +1,349 @@
+package utils;
+
+/**
+ * <h3>类名：该类用于xxx</h3>
+ * description typescript token
+ *
+ * @author Administrator
+ *         date 2025-02-28
+ * @version 1.0
+ * @since 2025-02-28
+ */
+
+public class TsToken {
+    /**
+     * enum token
+     */
+    public static final String TS_TOKEN_ENUM = "enum";
+
+    /**
+     * class token
+     */
+    public static final String TS_TOKEN_CLASS = "class";
+
+    /**
+     * extends token
+     */
+    public static final String TS_TOKEN_EXTENDS = "extends";
+
+    /**
+     * super token
+     */
+    public static final String TS_TOKEN_SUPER = "super";
+
+    /**
+     * const token
+     */
+    public static final String TS_TOKEN_CONST = "const";
+
+    /**
+     * export token
+     */
+    public static final String TS_TOKEN_EXPORT = "export";
+
+    /**
+     * import token
+     */
+    public static final String TS_TOKEN_IMPORT = "import";
+
+    /**
+     * implements token
+     */
+    public static final String TS_TOKEN_IMPLEMENT = "implements";
+
+    /**
+     * let token
+     */
+    public static final String TS_TOKEN_LET = "let";
+
+    /**
+     * private token
+     */
+    public static final String TS_TOKEN_PRIVATE = "private";
+
+    /**
+     * public token
+     */
+    public static final String TS_TOKEN_PUBLIC = "public";
+
+    /**
+     * interface token
+     */
+    public static final String TS_TOKEN_INTERFACE = "interface";
+
+    /**
+     * package token
+     */
+    public static final String TS_TOKEN_PACKAGE = "package";
+
+    /**
+     * protected token
+     */
+    public static final String TS_TOKEN_PROTECTED = "protected";
+
+    /**
+     * static token
+     */
+    public static final String TS_TOKEN_STATIC = "static";
+
+    /**
+     * any token
+     */
+    public static final String TS_TOKEN_ANY = "any";
+
+    /**
+     * number token
+     */
+    public static final String TS_TOKEN_NUMBER = "number";
+
+    /**
+     * never token
+     */
+    public static final String TS_TOKEN_NEVER = "never";
+
+    /**
+     * boolean token
+     */
+    public static final String TS_TOKEN_BOOLEAN = "boolean";
+
+    /**
+     * string token
+     */
+    public static final String TS_TOKEN_STRING = "string";
+
+    /**
+     * unique token
+     */
+    public static final String TS_TOKEN_UNIQUE = "unique";
+
+    /**
+     * symbol token
+     */
+    public static final String TS_TOKEN_SYMBOL = "symbol";
+
+    /**
+     * undefined token
+     */
+    public static final String TS_TOKEN_UNDEFINED = "undefined";
+
+    /**
+     * object token
+     */
+    public static final String TS_TOKEN_OBJECT = "object";
+
+    /**
+     * of token
+     */
+    public static final String TS_TOKEN_OF = "of";
+
+    /**
+     * keyof token
+     */
+    public static final String TS_TOKEN_KEYOF = "keyof";
+
+    /**
+     * type token
+     */
+    public static final String TS_TOKEN_TYPE = "type";
+
+    /**
+     * constructor token
+     */
+    public static final String TS_TOKEN_CONSTRUCTOR = "constructor";
+
+    /**
+     * namespace token
+     */
+    public static final String TS_TOKEN_NAMESPACE = "namespace";
+
+    /**
+     * require token
+     */
+    public static final String TS_TOKEN_REQUIRE = "require";
+
+    /**
+     * module token
+     */
+    public static final String TS_TOKEN_MODULE = "module";
+
+    /**
+     * declare token
+     */
+    public static final String TS_TOKEN_DECLARE = "declare";
+
+    /**
+     * abstract token
+     */
+    public static final String TS_TOKEN_ABSTRACT = "abstract";
+
+    /**
+     * IS token
+     */
+    public static final String TS_TOKEN_IS = "is";
+
+    /**
+     * null token
+     */
+    public static final String TS_TOKEN_NULL = "null";
+
+    /**
+     * break token
+     */
+    public static final String TS_TOKEN_BREAK = "break";
+
+    /**
+     * do token
+     */
+    public static final String TS_TOKEN_DO = "do";
+
+    /**
+     * instanceof token
+     */
+    public static final String TS_TOKEN_INSTANCEOF = "instanceof";
+
+    /**
+     * typeof token
+     */
+    public static final String TS_TOKEN_TYPEOF = "typeof";
+
+    /**
+     * case token
+     */
+    public static final String TS_TOKEN_CASE = "case";
+
+    /**
+     * else token
+     */
+    public static final String TS_TOKEN_ELSE = "else";
+
+    /**
+     * new token
+     */
+    public static final String TS_TOKEN_NEW = "new";
+
+    /**
+     * var token
+     */
+    public static final String TS_TOKEN_VAR = "var";
+
+    /**
+     * catch token
+     */
+    public static final String TS_TOKEN_CATCH = "catch";
+
+    /**
+     * finally token
+     */
+    public static final String TS_TOKEN_FINALLY = "finally";
+
+    /**
+     * return token
+     */
+    public static final String TS_TOKEN_RETURN = "return";
+
+    /**
+     * void token
+     */
+    public static final String TS_TOKEN_VOID = "void";
+
+    /**
+     * continue token
+     */
+    public static final String TS_TOKEN_CONTINUE = "continue";
+
+    /**
+     * for token
+     */
+    public static final String TS_TOKEN_FOR = "for";
+
+    /**
+     * switch token
+     */
+    public static final String TS_TOKEN_SWITCH = "switch";
+
+    /**
+     * while token
+     */
+    public static final String TS_TOKEN_WHILE = "while";
+
+    /**
+     * debugger token
+     */
+    public static final String TS_TOKEN_DEBUGGER = "debugger";
+
+    /**
+     * function token
+     */
+    public static final String TS_TOKEN_FUNCTION = "function";
+
+    /**
+     * this token
+     */
+    public static final String TS_TOKEN_THIS = "this";
+
+    /**
+     * with token
+     */
+    public static final String TS_TOKEN_WITH = "with";
+
+    /**
+     * default token
+     */
+    public static final String TS_TOKEN_DEFAULT = "default";
+
+    /**
+     * if token
+     */
+    public static final String TS_TOKEN_IF = "if";
+
+    /**
+     * throw token
+     */
+    public static final String TS_TOKEN_THROW = "throw";
+
+    /**
+     * delete token
+     */
+    public static final String TS_TOKEN_DELETE = "delete";
+
+    /**
+     * in token
+     */
+    public static final String TS_TOKEN_IN = "in";
+
+    /**
+     * try token
+     */
+    public static final String TS_TOKEN_TRY = "try";
+
+    /**
+     * as token
+     */
+    public static final String TS_TOKEN_AS = "as";
+
+    /**
+     * from token
+     */
+    public static final String TS_TOKEN_FROM = "from";
+
+    /**
+     * readonly token
+     */
+    public static final String TS_TOKEN_READONLY = "readonly";
+
+    /**
+     * async token
+     */
+    public static final String TS_TOKEN_ASYNC = "async";
+
+    /**
+     * await token
+     */
+    public static final String TS_TOKEN_AWAIT = "await";
+
+    /**
+     * yield token
+     */
+    public static final String TS_TOKEN_YIELD = "yield";
+
+}
