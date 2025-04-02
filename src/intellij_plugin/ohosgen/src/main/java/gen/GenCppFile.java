fd9c2ea0caf9d5a24e1f8997e1fb7dc236c88f3c
wangshi <wangshi@kaihong.com>
2025-04-02 14:13:02 +0800

    add cpp gen
    Signed-off-by: wangshi <wangshi@kaihong.com>

--- src/intellij_plugin/ohosgen/src/main/java/gen/GenCppFile.java
+++ src/intellij_plugin/ohosgen/src/main/java/gen/GenCppFile.java
@@ -15,6 +15,10 @@
 
 package gen;
 
+import grammar.*;
+
+import java.util.List;
+
 /**
  * <h3>类名：该类用于xxx</h3>
  * description ${description}
@@ -24,5 +28,89 @@ package gen;
  * @since 2025-02-28
  * @version 1.0
  */
-public class GenCppFile {
+public class GenCppFile extends GeneratorBase {
+    /**
+     * 构造函数
+     */
+    GenCppFile() {
+
+    }
+
+    /**
+     * 生成输出内容
+     * @param po
+     */
+    @Override
+    public void genContent(ParseObj po) {
+        genInterfaceList(po.getInterfaceList());
+        genEnumList(po.getEnumList());
+        genClassList(po.getClassList());
+        genFuncList(po.getFuncList());
+        genStructList(po.getStructList());
+        genTypeList(po.getTypeList());
+        genUnionList(po.getUnionList());
+    }
+
+    /**
+     * 生成输出内容
+     * @param iol
+     */
+    @Override
+    public void genInterfaceList(List<InterfaceObject> iol) {
+
+    };
+
+    /**
+     * 生成输出内容
+     * @param eol
+     */
+    @Override
+    public void genEnumList(List<EnumObj> eol) {
+
+    };
+
+    /**
+     * 生成输出内容
+     * @param col
+     */
+    @Override
+    public void genClassList(List<ClassObj> col) {
+
+    };
+
+    /**
+     * 生成输出内容
+     * @param fol
+     */
+    @Override
+    public void genFuncList(List<FuncObj> fol) {
+
+    };
+
+    /**
+     * 生成输出内容
+     * @param sol
+     */
+    @Override
+    public void genStructList(List<StructObj> sol) {
+
+    };
+
+    /**
+     * 生成输出内容
+     * @param tol
+     */
+    @Override
+    public void genTypeList(List<TypeObj> tol) {
+
+    };
+
+    /**
+     * 生成输出内容
+     * @param uol
+     */
+    @Override
+    public void genUnionList(List<UnionObj> uol) {
+
+    };
 }
