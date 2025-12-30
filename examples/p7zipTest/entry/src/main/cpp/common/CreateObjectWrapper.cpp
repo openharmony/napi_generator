5a8f9bf00efcb90e4e2eeda37b83a332319c723d
gou-jingjing <goujingjing@kaihong.com>
2025-12-30 11:46:05 +0800

    fix codecheck
    Signed-off-by: gou-jingjing <goujingjing@kaihong.com>

--- examples/p7zipTest/entry/src/main/cpp/common/CreateObjectWrapper.cpp
+++ examples/p7zipTest/entry/src/main/cpp/common/CreateObjectWrapper.cpp
@@ -1,7 +1,7 @@
 // CreateObjectWrapper.cpp - CreateObject 函数包装
 
-#include "Common/MyWindows.h"
 #include "Common/MyUnknown.h"
+#include "Common/MyWindows.h"
 #include "hilog/log.h"
 #include <cstring>
 
@@ -13,19 +13,19 @@
 // 尝试链接 lib7z.a 中的各种可能的 CreateObject 符号
 // 1. 尝试 C 链接
 extern "C" {
-    // 如果库中定义为 C 函数
-    HRESULT CreateObject(const GUID *clsid, const GUID *iid, void **outObject) __attribute__((weak));
+// 如果库中定义为 C 函数
+HRESULT CreateObject(const GUID *clsid, const GUID *iid, void **outObject) __attribute__((weak));
 }
 
 // 2. 尝试 C++ 链接（可能在命名空间中）
 namespace NArchive {
 namespace N7z {
-    HRESULT CreateObject(const GUID *clsid, const GUID *iid, void **outObject) __attribute__((weak));
+HRESULT CreateObject(const GUID *clsid, const GUID *iid, void **outObject) __attribute__((weak));
 }
 namespace NZip {
-    HRESULT CreateObject(const GUID *clsid, const GUID *iid, void **outObject) __attribute__((weak));
+HRESULT CreateObject(const GUID *clsid, const GUID *iid, void **outObject) __attribute__((weak));
 }
-}
+} // namespace NArchive
 
 // 如果以上都没有定义，提供一个基本的实现
 // 这个实现会尝试调用库中的其他可能的入口点
@@ -35,28 +35,28 @@ extern "C" HRESULT CreateObject(const GUID *clsid, const GUID *iid, void **outOb
         OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, LOG_TAG, "CreateObject: Invalid arguments");
         return E_INVALIDARG;
     }
-    
+
     *outObject = nullptr;
-    
+
     // 对于 7z 格式
     static const GUID CLSID_CFormat7z = {0x23170F69, 0x40C1, 0x278A, {0x10, 0x00, 0x00, 0x01, 0x10, 0x07, 0x00, 0x00}};
-    // 对于 ZIP 格式  
+    // 对于 ZIP 格式
     static const GUID CLSID_CFormatZip = {0x23170F69, 0x40C1, 0x278A, {0x10, 0x00, 0x00, 0x01, 0x10, 0x01, 0x00, 0x00}};
-    
+
     // 检测格式
     bool is7z = (memcmp(clsid, &CLSID_CFormat7z, sizeof(GUID)) == 0);
     bool isZip = (memcmp(clsid, &CLSID_CFormatZip, sizeof(GUID)) == 0);
-    
+
     // 检查弱符号
-    extern HRESULT CreateArchiver(const GUID*, const GUID*, void**) __attribute__((weak));
-    
+    extern HRESULT CreateArchiver(const GUID *, const GUID *, void **) __attribute__((weak));
+
     // 尝试从命名空间调用
     if (is7z && NArchive::N7z::CreateObject) {
         return NArchive::N7z::CreateObject(clsid, iid, outObject);
     } else if (isZip && NArchive::NZip::CreateObject) {
         return NArchive::NZip::CreateObject(clsid, iid, outObject);
     }
-    
+
     // 如果命名空间版本不可用，尝试全局 CreateArchiver
     static bool in_fallback = false;
     if (!in_fallback && CreateArchiver != nullptr) {
@@ -65,9 +65,8 @@ extern "C" HRESULT CreateObject(const GUID *clsid, const GUID *iid, void **outOb
         in_fallback = false;
         return result;
     }
-    
+
     OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, LOG_TAG, "CreateObject failed: No valid implementation found");
     return E_NOTIMPL;
 }
 #endif
-
--- examples/p7zipTest/entry/src/main/cpp/common/FormatDetector.cpp
+++ examples/p7zipTest/entry/src/main/cpp/common/FormatDetector.cpp
@@ -1,8 +1,8 @@
 #include "FormatDetector.h"
 #include "common.h"
-#include <fstream>
 #include <algorithm>
 #include <cstring>
+#include <fstream>
 
 // 文件魔数定义
 static const unsigned char MAGIC_7Z[] = {0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C};
@@ -20,46 +20,60 @@ static const unsigned char MAGIC_CAB[] = {0x4D, 0x53, 0x43, 0x46};
 static const unsigned char MAGIC_WIM[] = {0x4D, 0x53, 0x57, 0x49, 0x4D, 0x00, 0x00, 0x00}; // "MSWIM\0\0\0"
 
 // 读取文件头数据
-static bool ReadFileHeader(const std::string& filePath, unsigned char* header, 
-                          size_t headerSize, size_t& bytesRead) {
+static bool ReadFileHeader(const std::string &filePath, unsigned char *header, size_t headerSize, size_t &bytesRead) {
     std::ifstream file(filePath, std::ios::binary);
     if (!file.good()) {
         return false;
     }
-    file.read((char*)header, headerSize);
+    file.read((char *)header, headerSize);
     bytesRead = file.gcount();
     file.close();
     return bytesRead >= MIN_HEADER_SIZE;
 }
 
 // 检查基本格式魔数
-static ArchiveFormat CheckBasicFormats(const unsigned char* header, size_t bytesRead) {
-    if (bytesRead >= MAGIC_7Z_SIZE && memcmp(header, MAGIC_7Z, MAGIC_7Z_SIZE) == 0) return ArchiveFormat::SEVENZ;
-    
+static ArchiveFormat CheckBasicFormats(const unsigned char *header, size_t bytesRead) {
+    if (bytesRead >= MAGIC_7Z_SIZE && memcmp(header, MAGIC_7Z, MAGIC_7Z_SIZE) == 0) {
+        return ArchiveFormat::SEVENZ;
+    }
+
     if (bytesRead >= MAGIC_ZIP_SIZE) {
         if (memcmp(header, MAGIC_ZIP, MAGIC_ZIP_SIZE) == 0 || memcmp(header, MAGIC_ZIP_EMPTY, MAGIC_ZIP_SIZE) == 0 ||
             memcmp(header, MAGIC_ZIP_SPANNED, MAGIC_ZIP_SIZE) == 0) {
             return ArchiveFormat::ZIP;
         }
     }
-    
-    if (bytesRead >= MAGIC_RAR5_SIZE && memcmp(header, MAGIC_RAR5, MAGIC_RAR5_SIZE) == 0) return ArchiveFormat::RAR5;
-    if (bytesRead >= MAGIC_RAR_SIZE && memcmp(header, MAGIC_RAR, MAGIC_RAR_SIZE) == 0) return ArchiveFormat::RAR;
-    if (bytesRead >= MAGIC_GZIP_SIZE && memcmp(header, MAGIC_GZIP, MAGIC_GZIP_SIZE) == 0) return ArchiveFormat::GZIP;
-    if (bytesRead >= MAGIC_BZIP2_SIZE && memcmp(header, MAGIC_BZIP2, MAGIC_BZIP2_SIZE) == 0)
+
+    if (bytesRead >= MAGIC_RAR5_SIZE && memcmp(header, MAGIC_RAR5, MAGIC_RAR5_SIZE) == 0) {
+        return ArchiveFormat::RAR5;
+    }
+    if (bytesRead >= MAGIC_RAR_SIZE && memcmp(header, MAGIC_RAR, MAGIC_RAR_SIZE) == 0) {
+        return ArchiveFormat::RAR;
+    }
+    if (bytesRead >= MAGIC_GZIP_SIZE && memcmp(header, MAGIC_GZIP, MAGIC_GZIP_SIZE) == 0) {
+        return ArchiveFormat::GZIP;
+    }
+    if (bytesRead >= MAGIC_BZIP2_SIZE && memcmp(header, MAGIC_BZIP2, MAGIC_BZIP2_SIZE) == 0) {
         return ArchiveFormat::BZIP2;
-    if (bytesRead >= MAGIC_XZ_SIZE && memcmp(header, MAGIC_XZ, MAGIC_XZ_SIZE) == 0) return ArchiveFormat::XZ;
-    if (bytesRead >= MAGIC_LZMA_SIZE && header[0] == 0x5D && header[1] == 0x00 && header[2] == 0x00) {
+    }
+    if (bytesRead >= MAGIC_XZ_SIZE && memcmp(header, MAGIC_XZ, MAGIC_XZ_SIZE) == 0) {
+        return ArchiveFormat::XZ;
+    }
+    if (bytesRead >= MAGIC_LZMA_SIZE && header[0] == 0x5D && header[1] == 0x00 && header[LZMA_HEADER_INDEX_2] == 0x00) {
         return ArchiveFormat::LZMA;
     }
-    if (bytesRead >= MAGIC_CAB_SIZE && memcmp(header, MAGIC_CAB, MAGIC_CAB_SIZE) == 0) return ArchiveFormat::CAB;
-    if (bytesRead >= MAGIC_WIM_SIZE && memcmp(header, MAGIC_WIM, MAGIC_WIM_SIZE) == 0) return ArchiveFormat::WIM;
-    
+    if (bytesRead >= MAGIC_CAB_SIZE && memcmp(header, MAGIC_CAB, MAGIC_CAB_SIZE) == 0) {
+        return ArchiveFormat::CAB;
+    }
+    if (bytesRead >= MAGIC_WIM_SIZE && memcmp(header, MAGIC_WIM, MAGIC_WIM_SIZE) == 0) {
+        return ArchiveFormat::WIM;
+    }
+
     return ArchiveFormat::UNKNOWN;
 }
 
 // 检查TAR格式
-static ArchiveFormat CheckTarFormat(const std::string& filePath) {
+static ArchiveFormat CheckTarFormat(const std::string &filePath) {
     std::ifstream tarFile(filePath, std::ios::binary);
     tarFile.seekg(TAR_USTAR_OFFSET);
     char ustar[ARRAY_SIZE_SIX] = {0};
@@ -68,16 +82,18 @@ static ArchiveFormat CheckTarFormat(const std::string& filePath) {
 }
 
 // 检查ISO格式
-static ArchiveFormat CheckIsoFormat(const std::string& filePath) {
+static ArchiveFormat CheckIsoFormat(const std::string &filePath) {
     std::ifstream isoFile(filePath, std::ios::binary);
-    if (!isoFile.good()) return ArchiveFormat::UNKNOWN;
-    
+    if (!isoFile.good()) {
+        return ArchiveFormat::UNKNOWN;
+    }
+
     const size_t offsets[] = {ISO_MAGIC_OFFSET_1, ISO_MAGIC_OFFSET_2, ISO_MAGIC_OFFSET_3};
     unsigned char cdMagic[ISO_MAGIC_SIZE];
-    
+
     for (size_t offset : offsets) {
         isoFile.seekg(offset);
-        isoFile.read((char*)cdMagic, ISO_MAGIC_SIZE);
+        isoFile.read((char *)cdMagic, ISO_MAGIC_SIZE);
         if (isoFile.gcount() == ISO_MAGIC_SIZE && memcmp(cdMagic, MAGIC_ISO, ISO_MAGIC_SIZE) == 0) {
             return ArchiveFormat::ISO;
         }
@@ -85,89 +101,129 @@ static ArchiveFormat CheckIsoFormat(const std::string& filePath) {
     return ArchiveFormat::UNKNOWN;
 }
 
-ArchiveFormat FormatDetector::DetectBySignature(const std::string& filePath)
-{
+ArchiveFormat FormatDetector::DetectBySignature(const std::string &filePath) {
     unsigned char header[HEADER_BUFFER_SIZE] = {0};
     size_t bytesRead = 0;
-    
+
     if (!ReadFileHeader(filePath, header, sizeof(header), bytesRead)) {
         return ArchiveFormat::UNKNOWN;
     }
-    
+
     ArchiveFormat format = CheckBasicFormats(header, bytesRead);
-    if (format != ArchiveFormat::UNKNOWN) return format;
-    
+    if (format != ArchiveFormat::UNKNOWN) {
+        return format;
+    }
+
     if (bytesRead >= TAR_USTAR_CHECK_SIZE) {
         format = CheckTarFormat(filePath);
-        if (format != ArchiveFormat::UNKNOWN) return format;
+        if (format != ArchiveFormat::UNKNOWN) {
+            return format;
+        }
     }
-    
+
     return CheckIsoFormat(filePath);
 }
 
-ArchiveFormat FormatDetector::DetectByExtension(const std::string& filePath)
-{
+ArchiveFormat FormatDetector::DetectByExtension(const std::string &filePath) {
     // 获取小写扩展名
     size_t dotPos = filePath.find_last_of('.');
     if (dotPos == std::string::npos) {
         return ArchiveFormat::UNKNOWN;
     }
-    
+
     std::string ext = filePath.substr(dotPos + 1);
     std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
-    
+
     // 匹配扩展名
-    if (ext == "7z") return ArchiveFormat::SEVENZ;
-    if (ext == "zip" || ext == "jar" || ext == "apk") return ArchiveFormat::ZIP;
-    if (ext == "rar") return ArchiveFormat::RAR;
-    if (ext == "gz" || ext == "gzip") return ArchiveFormat::GZIP;
-    if (ext == "bz2" || ext == "bzip2") return ArchiveFormat::BZIP2;
-    if (ext == "xz") return ArchiveFormat::XZ;
-    if (ext == "lzma") return ArchiveFormat::LZMA;
-    if (ext == "lzma86") return ArchiveFormat::LZMA86;
-    if (ext == "tar") return ArchiveFormat::TAR;
-    if (ext == "iso" || ext == "img") return ArchiveFormat::ISO;
-    if (ext == "cab") return ArchiveFormat::CAB;
-    if (ext == "wim" || ext == "swm" || ext == "esd") return ArchiveFormat::WIM;
-    
+    if (ext == "7z") {
+        return ArchiveFormat::SEVENZ;
+    }
+    if (ext == "zip" || ext == "jar" || ext == "apk") {
+        return ArchiveFormat::ZIP;
+    }
+    if (ext == "rar") {
+        return ArchiveFormat::RAR;
+    }
+    if (ext == "gz" || ext == "gzip") {
+        return ArchiveFormat::GZIP;
+    }
+    if (ext == "bz2" || ext == "bzip2") {
+        return ArchiveFormat::BZIP2;
+    }
+    if (ext == "xz") {
+        return ArchiveFormat::XZ;
+    }
+    if (ext == "lzma") {
+        return ArchiveFormat::LZMA;
+    }
+    if (ext == "lzma86") {
+        return ArchiveFormat::LZMA86;
+    }
+    if (ext == "tar") {
+        return ArchiveFormat::TAR;
+    }
+    if (ext == "iso" || ext == "img") {
+        return ArchiveFormat::ISO;
+    }
+    if (ext == "cab") {
+        return ArchiveFormat::CAB;
+    }
+    if (ext == "wim" || ext == "swm" || ext == "esd") {
+        return ArchiveFormat::WIM;
+    }
+
     // 检查复合扩展名 (.tar.gz, .tar.bz2, etc.)
-    if (ext == "tgz" || ext == "tpz") return ArchiveFormat::GZIP;
-    if (ext == "tbz" || ext == "tbz2") return ArchiveFormat::BZIP2;
-    if (ext == "txz") return ArchiveFormat::XZ;
-    
+    if (ext == "tgz" || ext == "tpz")
+        return ArchiveFormat::GZIP;
+    if (ext == "tbz" || ext == "tbz2")
+        return ArchiveFormat::BZIP2;
+    if (ext == "txz")
+        return ArchiveFormat::XZ;
+
     return ArchiveFormat::UNKNOWN;
 }
 
-ArchiveFormat FormatDetector::Detect(const std::string& filePath)
-{
+ArchiveFormat FormatDetector::Detect(const std::string &filePath) {
     // 优先通过魔数检测
     ArchiveFormat format = DetectBySignature(filePath);
-    
+
     // 如果魔数检测失败，使用扩展名
     if (format == ArchiveFormat::UNKNOWN) {
         format = DetectByExtension(filePath);
     }
-    
+
     return format;
 }
 
-std::string FormatDetector::GetFormatName(ArchiveFormat format)
-{
+std::string FormatDetector::GetFormatName(ArchiveFormat format) {
     switch (format) {
-        case ArchiveFormat::SEVENZ: return "7z";
-        case ArchiveFormat::ZIP: return "Zip";
-        case ArchiveFormat::RAR: return "RAR";
-        case ArchiveFormat::RAR5: return "RAR5";
-        case ArchiveFormat::GZIP: return "Gzip";
-        case ArchiveFormat::BZIP2: return "Bzip2";
-        case ArchiveFormat::XZ: return "XZ";
-        case ArchiveFormat::LZMA: return "LZMA";
-        case ArchiveFormat::TAR: return "Tar";
-        case ArchiveFormat::ISO: return "ISO";
-        case ArchiveFormat::CAB: return "CAB";
-        case ArchiveFormat::WIM: return "WIM";
-        case ArchiveFormat::LZMA86: return "LZMA86";
-        default: return "Unknown";
+    case ArchiveFormat::SEVENZ:
+        return "7z";
+    case ArchiveFormat::ZIP:
+        return "Zip";
+    case ArchiveFormat::RAR:
+        return "RAR";
+    case ArchiveFormat::RAR5:
+        return "RAR5";
+    case ArchiveFormat::GZIP:
+        return "Gzip";
+    case ArchiveFormat::BZIP2:
+        return "Bzip2";
+    case ArchiveFormat::XZ:
+        return "XZ";
+    case ArchiveFormat::LZMA:
+        return "LZMA";
+    case ArchiveFormat::TAR:
+        return "Tar";
+    case ArchiveFormat::ISO:
+        return "ISO";
+    case ArchiveFormat::CAB:
+        return "CAB";
+    case ArchiveFormat::WIM:
+        return "WIM";
+    case ArchiveFormat::LZMA86:
+        return "LZMA86";
+    default:
+        return "Unknown";
     }
 }
-
