19971f0f4b829b54ee0f18deda03f335138a89bb
goujingjing <goujingjing@kaihong.com>
2026-08-14 02:14:59 +0000

    fix(perf): replace ambiguous v8 word in enum test cases
    Signed-off-by: goujingjing <goujingjing@kaihong.com>

--- src/vscode_plugin/src/test/suite/performance/conversion_cenum.part02.test.ts
+++ src/vscode_plugin/src/test/suite/performance/conversion_cenum.part02.test.ts
@@ -1338,7 +1338,7 @@ suite('Performance_C_Enum_Suite', function ()
     M5 = "v5",
     M6 = "v6",
     M7 = "v7",
-    M8 = "v8"
+    M8 = "x8"
 } En09F2;`);
         }
       });
@@ -1355,7 +1355,7 @@ suite('Performance_C_Enum_Suite', function ()
       assert.strictEqual(objList[0].members[5], 'M5="v5"');
       assert.strictEqual(objList[0].members[6], 'M6="v6"');
       assert.strictEqual(objList[0].members[7], 'M7="v7"');
-      assert.strictEqual(objList[0].members[8], 'M8="v8"');
+      assert.strictEqual(objList[0].members[8], 'M8="x8"');
       assert.ok(
         elapsed < PARSE_TOTAL_MS,
         `c_enum_0058 总耗时 ${elapsed}ms 超过阈值 ${PARSE_TOTAL_MS}ms（次数 ${localLoop}）`
@@ -1542,7 +1542,7 @@ suite('Performance_C_Enum_Suite', function ()
     M5 = "v5",
     M6 = "v6",
     M7 = "v7",
-    M8 = "v8",
+    M8 = "x8",
     M9 = "v9"
 } En10F2;`);
         }
@@ -1560,7 +1560,7 @@ suite('Performance_C_Enum_Suite', function ()
       assert.strictEqual(objList[0].members[5], 'M5="v5"');
       assert.strictEqual(objList[0].members[6], 'M6="v6"');
       assert.strictEqual(objList[0].members[7], 'M7="v7"');
-      assert.strictEqual(objList[0].members[8], 'M8="v8"');
+      assert.strictEqual(objList[0].members[8], 'M8="x8"');
       assert.strictEqual(objList[0].members[9], 'M9="v9"');
       assert.ok(
         elapsed < PARSE_TOTAL_MS,
@@ -1754,7 +1754,7 @@ suite('Performance_C_Enum_Suite', function ()
     M5 = "v5",
     M6 = "v6",
     M7 = "v7",
-    M8 = "v8",
+    M8 = "x8",
     M9 = "v9",
     M10 = "v10"
 } En11F2;`);
@@ -1773,7 +1773,7 @@ suite('Performance_C_Enum_Suite', function ()
       assert.strictEqual(objList[0].members[5], 'M5="v5"');
       assert.strictEqual(objList[0].members[6], 'M6="v6"');
       assert.strictEqual(objList[0].members[7], 'M7="v7"');
-      assert.strictEqual(objList[0].members[8], 'M8="v8"');
+      assert.strictEqual(objList[0].members[8], 'M8="x8"');
       assert.strictEqual(objList[0].members[9], 'M9="v9"');
       assert.strictEqual(objList[0].members[10], 'M10="v10"');
       assert.ok(
